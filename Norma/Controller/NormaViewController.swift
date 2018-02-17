44b24d5931d95a8cc1815f48098506438eabaf8b
Korman Chen <kormanchen@Kormans-MacBook-Pro.local>
2018-02-17 00:00:35 -0800

    best app ever

---
+++ Norma/Controller/NormaViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NormaViewController.swift
+//  Norma
+//
+//  Created by Korman Chen on 2/16/18.
+//  Copyright © 2018 Korman Chen. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class NormaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Norma' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Norma
+
+pod 'Firebase/Core'
+pod 'Alamofire'
+pod 'SwiftyJSON'
+
+end
