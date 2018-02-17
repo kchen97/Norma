7b1ffad721f7ee7a36171d488f9b7b5b430deee7
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-02-17 11:35:41 -0800

    ChatBot ViewController

---
+++ Norma/Controller/ChatBotViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatBotViewController.swift
+//  Norma
+//
+//  Created by Tim Roesner on 2/17/18.
+//  Copyright © 2018 Korman Chen. All rights reserved.
+//
+
+import UIKit
+
+class ChatBotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
